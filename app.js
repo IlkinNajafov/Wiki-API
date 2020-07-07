bcfb1b4c7a39361e0de13f2b68732d1f69871828
IlkinNajafov <58183512+IlkinNajafov@users.noreply.github.com>
2020-07-07 22:29:54 +0400

    post.delete added

--- app.js
+++ app.js
@@ -50,7 +50,17 @@ app.post("/articles", function (req, res) {
       res.send(err)
     }
   });
-})
+});
+
+app.delete("/articles", function (req, res) {
+  Article.deleteMany(function (err) {
+    if(!err){
+      console.log("Successfully deleted all articles.");
+    }else{
+      console.log(err);
+    }
+  });
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
