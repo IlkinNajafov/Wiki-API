8d53ae7900661d9c44e97186e79841846bdd5f1a
IlkinNajafov <58183512+IlkinNajafov@users.noreply.github.com>
2020-07-13 01:45:20 +0400

    delete, put, patch, get routes added to code and had some changes in database

--- app.js
+++ app.js
@@ -60,6 +60,58 @@ app.route("/articles")
   });
 });
 
+app.route("/articles/:articleTitle")
+
+.get(function (req, res) {
+
+  
+    Article.findOne({title: req.params.articleTitle}, function (err, foundArticle){
+      if (foundArticle){
+        res.send(foundArticle);
+      } else{
+        res.send("No articles found.");
+      }
+    });
+})
+
+.put(function (req, res) {
+    Article.update(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content},
+      {overwrite: true},
+      function (err, results) {
+        if (!err) {
+          res.send("Successfully updated article.");
+        }
+      }
+    );
+})
+.patch(function (req, res) {
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function (err) {
+      if (!err) {
+        res.send("Successfully updated article.");
+      } else{
+        res.send(err );
+      }
+    }
+  );  
+})
+.app.delete(function (req, res) {
+  Article.deleteOne(
+    {title: req.body.articleTitle, },
+    function (err) {
+      if (!err) {
+        res.send("Successfully deleted article.");
+      } else{
+        res.send(err);
+      }
+    }
+  );
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
